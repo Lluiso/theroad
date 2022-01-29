3c7c2e9dece044010adc0b048ea37546cbfc7524
Alexey <alexey@viker.co.uk>
2022-01-29 21:27:16 +0000

    spawn road signs

--- Assets/Scripts/RoadSection.cs
+++ Assets/Scripts/RoadSection.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 
 public class RoadSection : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Plane _groundLeft;
 
-    // Update is called once per frame
-    void Update()
+    void PlaceVegetation()
     {
         
     }
--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -4,9 +4,12 @@ public class TerrainGenerator : MonoBehaviour
 {
 	[SerializeField] private GameSettings _settings;
 	[SerializeField] private float _segmentLength;
+	[SerializeField] private float _roadSignIntervalsMeters;
 	[SerializeField] private float _visibleLength;
 	[SerializeField] private GameObject _roadSectionPrefab;
+	[SerializeField] private GameObject _roadSignPrefab;
 	private int _numberOfSectionsToGenerate;
+	private Transform _roadSegmentsParent;
 
 	public void DEBUGClearAndGenerate()
 	{
@@ -17,7 +20,38 @@ public class TerrainGenerator : MonoBehaviour
 
 	void Generate()
 	{
+		SpawnRoadSegments();
+		SpawnRoadSigns();
+	}
+
+	void SpawnRoadSigns()
+	{
+		var container = MakeNewContainer("Road Signs");
+		var currentDistance = 0f;
+		while (currentDistance < _settings.StartingDistanceToFerryMeters)
+		{
+			currentDistance += _roadSignIntervalsMeters;
+			var distanceRemaining =
+				Mathf.RoundToInt((_settings.StartingDistanceToFerryMeters - currentDistance) / 1000);
+			if (distanceRemaining != 0)
+			{
+				var newSignGO = Instantiate(_roadSignPrefab);
+				newSignGO.name = $"{distanceRemaining} km";
+				newSignGO.transform.parent = container;
+				var newPos = newSignGO.transform.position;
+				newPos.z = currentDistance;
+				newSignGO.transform.position = newPos;
+				newSignGO.GetComponent<SetText>().Set($"Ferry \n{distanceRemaining} km");
+			}
+		}
+	}
+
+	void SpawnRoadSegments()
+	{
+		_roadSegmentsParent = MakeNewContainer("Road Segments");
 		float nextZ = transform.position.z;
+
+		// road & ground sections
 		for (int i = 0; i < _numberOfSectionsToGenerate; i++)
 		{
 			nextZ = _segmentLength * i;
@@ -26,7 +60,16 @@ public class TerrainGenerator : MonoBehaviour
 			var newRoadPiece = Instantiate(_roadSectionPrefab, transform, true);
 			newRoadPiece.name = $"Road section {i}";
 			newRoadPiece.transform.position = newSpawnPos;
+			newRoadPiece.transform.parent = _roadSegmentsParent;
 		}
+	}
+
+	Transform MakeNewContainer(string newName)
+	{
+		var go = new GameObject(newName).transform;
+		go.transform.position = Vector3.zero;
+		go.parent = transform;
+		return go;
 	}
 
 	void DestroyChildren()
