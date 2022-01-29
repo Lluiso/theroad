bad83e6c1f0a53c5427bf51b9d8834d1c914d1da
Alexey <alexey@viker.co.uk>
2022-01-29 01:36:26 +0000

    basic road

---
+++ Assets/Editor/TerrainGeneratorInspector.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(TerrainGenerator))]
+public class TerrainGeneratorInspector : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		TerrainGenerator generator = (TerrainGenerator)target;
+		base.OnInspectorGUI();
+		if (GUILayout.Button("Clear and generate"))
+		{
+			generator.DEBUGClearAndGenerate();
+		}
+	}
+}
--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -1,7 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TerrainGenerator : MonoBehaviour
 {
+	[SerializeField] private int _sectionsToGenerate;
+	[SerializeField] private int _sectionLengthWorldUnits;
+	[SerializeField] private GameObject _roadSectionPrefab;
+
+	public void DEBUGClearAndGenerate()
+	{
+		DestroyChildren();
+		Generate();
+	}
+
+	void Generate()
+	{
+		float nextZ = transform.position.z;
+		for (int i = 0; i < _sectionsToGenerate; i++)
+		{
+			nextZ = _sectionLengthWorldUnits * i;
+			var newSpawnPos = transform.position;
+			newSpawnPos.z = nextZ;
+			var newRoadPiece = Instantiate(_roadSectionPrefab, transform, true);
+			newRoadPiece.transform.position = newSpawnPos;
+		}
+	}
+
+	void DestroyChildren()
+	{
+		for (int i = transform.childCount - 1; i >= 0; i--)
+		{
+			DestroyImmediate(transform.GetChild(i).gameObject);
+		}
+	}
 }
