1d5b3eea1f98f99e61094faf3e170f036843ed52
Alexey <alexey@viker.co.uk>
2022-01-30 16:22:59 +0000

    tidyup

--- Assets/Scripts/TrackGenerator.cs
+++ Assets/Scripts/TrackGenerator.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using DG.Tweening;
 using TeamDuaLipa;
 using UnityEngine;
 using UnityEngine.SceneManagement;
